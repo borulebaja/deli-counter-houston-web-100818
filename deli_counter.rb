25750911fcd0bb4f03d11656ea47bd5ed0a5624e
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-29 17:56:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,13 @@
 katz_deli = []
 
-def line(katz_deli) 
-    phrase = "The line is currently:"
+def line(katz_deli)
+  phrase = "The line is currently: "
   if katz_deli.length > 0 
-    "The line is currently:"
-    katz_deli.each_with_index do |name, index| 
-    phrase += " #{index + 1}. #{name}"
+    katz_deli.each_with_index do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
   end
-  else 
-    puts "The line is currently empty"
-  end 
 end
-
