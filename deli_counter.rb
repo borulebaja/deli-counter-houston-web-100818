5b046c94744d8ce723e673110d994dbcbf3a216e
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-29 18:09:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,4 +15,5 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.push(name)
-    puts "Welcome #{name}! You are "
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
