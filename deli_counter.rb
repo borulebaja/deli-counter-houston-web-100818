93ac50ea592e5cb82b11c9b423035a884dc0433f
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-29 17:30:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 katz_deli = []
+
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end
